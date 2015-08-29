62e4755c44f0ab8a286aa57e9d734280512608af
Jay Hu <liang12hour@gmail.com>
2015-08-29 14:28:40 -0400

    add usrs page

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/create', function(req, res, next) {
+  var vm = {
+    title: 'Create an account'
+  };
+  res.render('users/create', vm);
+});
+
+router.post('/create', function(req, res, next) {
+  var error = true;
+  if(error) {
+    var vm = {
+      title: 'Create an account',
+      input: req.body,
+      error: "something went wrong"
+    };
+    delete vm.input.password;
+    return res.render('users/create', vm);
+  }
+  res.redirect('/orders');
+});
+
 module.exports = router;
