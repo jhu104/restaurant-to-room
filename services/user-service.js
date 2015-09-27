32bf246a3ba0096cb42bda816fc75f3b166c00b0
Jay Hu <liang12hour@gmail.com>
2015-09-27 07:42:20 -0400

    hash password

--- services/user-service.js
+++ services/user-service.js
@@ -1,20 +1,25 @@
 var User = require('../models/user').User;
-
+var bcrypt = require('bcrypt');
 exports.addUser = function (user, next) {
-  var newUser = new User({
-    firstName: user.firstName,
-    lastName: user.lastName,
-    roomNumber: user.roomNumber,
-    email: user.email.toLowerCase(),
-    password: user.password
-  });
-
-  newUser.save(function (err) {
-    if (err) {
-      return next(err);
+  bcrypt.hash(user.password, 10, function (err, hash) { //10 is cost factor in power of 2 so it will go through 1024 iterations
+    if(err) {
+      next(err);
     }
-    next(null);
-  });
+    var newUser = new User({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      roomNumber: user.roomNumber,
+      email: user.email.toLowerCase(),
+      password: hash
+    });
+
+    newUser.save(function (err) {
+      if (err) {
+        return next(err);
+      }
+      next(null);
+    });
+  }); 
 };
 
 exports.findUser = function (email, next) {
